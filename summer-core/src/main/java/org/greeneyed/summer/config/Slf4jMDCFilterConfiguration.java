df881f9253ad50e3478088855a6221ff9adcb0cb
verdoso <verdoso@gmail.com>
2018-04-16 10:00:33 +0200

    Added Client IP to filter

--- summer-core/src/main/java/org/greeneyed/summer/config/Slf4jMDCFilterConfiguration.java
+++ summer-core/src/main/java/org/greeneyed/summer/config/Slf4jMDCFilterConfiguration.java
@@ -58,15 +58,17 @@ public class Slf4jMDCFilterConfiguration {
 
     public static final String DEFAULT_RESPONSE_TOKEN_HEADER = "Response_Token";
     public static final String DEFAULT_MDC_UUID_TOKEN_KEY = "Slf4jMDCFilter.UUID";
+    public static final String DEFAULT_MDC_CLIENT_IP_KEY = "Slf4jMDCFilter.ClientIP";
 
     private String responseHeader = DEFAULT_RESPONSE_TOKEN_HEADER;
     private String mdcTokenKey = DEFAULT_MDC_UUID_TOKEN_KEY;
+    private String mdcClientIPKey = DEFAULT_MDC_CLIENT_IP_KEY;
     private String requestHeader = null;
 
     @Bean
     public FilterRegistrationBean servletRegistrationBean() {
         final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        final Slf4jMDCFilter log4jMDCFilterFilter = new Slf4jMDCFilter(responseHeader, mdcTokenKey, requestHeader);
+        final Slf4jMDCFilter log4jMDCFilterFilter = new Slf4jMDCFilter(responseHeader, mdcTokenKey, mdcClientIPKey, requestHeader);
         registrationBean.setFilter(log4jMDCFilterFilter);
         registrationBean.setOrder(2);
         return registrationBean;
--- summer-core/src/main/java/org/greeneyed/summer/filter/Slf4jMDCFilter.java
+++ summer-core/src/main/java/org/greeneyed/summer/filter/Slf4jMDCFilter.java
@@ -52,30 +52,30 @@ public class Slf4jMDCFilter extends OncePerRequestFilter {
 
     private final String responseHeader;
     private final String mdcTokenKey;
+    private final String mdcClientIPKey;
     private final String requestHeader;
 
     public Slf4jMDCFilter() {
         responseHeader = Slf4jMDCFilterConfiguration.DEFAULT_RESPONSE_TOKEN_HEADER;
         mdcTokenKey = Slf4jMDCFilterConfiguration.DEFAULT_MDC_UUID_TOKEN_KEY;
+        mdcClientIPKey = Slf4jMDCFilterConfiguration.DEFAULT_MDC_CLIENT_IP_KEY;
         requestHeader = null;
     }
 
-    public Slf4jMDCFilter(final String responseHeader, final String mdcTokenKey, final String requestHeader) {
+    public Slf4jMDCFilter(final String responseHeader, final String mdcTokenKey, final String mdcClientIPKey, final String requestHeader) {
         this.responseHeader = responseHeader;
         this.mdcTokenKey = mdcTokenKey;
+        this.mdcClientIPKey = mdcClientIPKey;
         this.requestHeader = requestHeader;
     }
 
     @Override
     protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
         throws java.io.IOException, ServletException {
-        try {
-            final String token;
-            if (!StringUtils.isEmpty(requestHeader) && !StringUtils.isEmpty(request.getHeader(requestHeader))) {
-                token = request.getHeader(requestHeader);
-            } else {
-                token = UUID.randomUUID().toString().toUpperCase().replace("-", "");
-            }
+        try {            
+            final String token = extractToken(request);
+            final String clientIP = extractClientIP(request);
+            MDC.put(mdcClientIPKey, clientIP);
             MDC.put(mdcTokenKey, token);
             if (!StringUtils.isEmpty(responseHeader)) {
                 response.addHeader(responseHeader, token);
@@ -83,7 +83,30 @@ public class Slf4jMDCFilter extends OncePerRequestFilter {
             chain.doFilter(request, response);
         } finally {
             MDC.remove(mdcTokenKey);
+            MDC.remove(mdcClientIPKey);
         }
+    }
+
+    private String extractToken(final HttpServletRequest request) {
+        final String token;
+        if (!StringUtils.isEmpty(requestHeader) && !StringUtils.isEmpty(request.getHeader(requestHeader))) {
+            token = request.getHeader(requestHeader);
+        } else {
+            token = UUID.randomUUID().toString().toUpperCase().replace("-", "");
+        }
+        return token;
+    }
+
+    private String extractClientIP(final HttpServletRequest request) {
+        final String clientIP;
+        if(request.getHeader("X-Forwarded-For")!=null)
+        {
+            clientIP = request.getHeader("X-Forwarded-For").split(",")[0];
+        }
+        else {
+            clientIP = request.getRemoteAddr();
+        }
+        return clientIP;
     }
 
     @Override
