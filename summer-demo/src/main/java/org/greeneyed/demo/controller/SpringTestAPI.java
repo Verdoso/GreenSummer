43a18e42aa1a330b3d1ea28187153d6824b8065f
verdoso <verdoso@gmail.com>
2018-08-23 10:03:28 +0200

    Added JOLT to demo testing

--- summer-demo/src/main/java/org/greeneyed/demo/controller/SpringTestAPI.java
+++ summer-demo/src/main/java/org/greeneyed/demo/controller/SpringTestAPI.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 import org.greeneyed.demo.model.App;
 import org.greeneyed.demo.model.MyPojo;
+import org.greeneyed.summer.config.JoltViewConfiguration.JoltModelAndView;
 import org.greeneyed.summer.config.XsltConfiguration.XsltModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,20 +24,34 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 public class SpringTestAPI {
 
-    @Autowired
-    private ConfigurableEnvironment env;
+	@Autowired
+	private ConfigurableEnvironment env;
 
-    @RequestMapping(value = "/test")
-    public ModelAndView testInterface() {
-        App app = new App(Arrays.asList(new MyPojo("anId", "aName"), new MyPojo("anotherId", "anotherName")));
-        log.debug("Testing");
-        return new XsltModelAndView("pojo_process", app, HttpStatus.OK);
-    }
+	@RequestMapping(value = "/test")
+	public ModelAndView testInterface() {
+		return new XsltModelAndView("pojo_process", generateAppObject(), HttpStatus.OK);
+	}
 
-    @RequestMapping(value = "/param_test")
-    public ResponseEntity<String> testParameters(@RequestParam(name = "name_x") String nameString, @RequestParam(name = "name_int") int nameInteger,
-        @RequestParam(name = "name_array") String[] theValues) {
-        //throw new RuntimeException("Probando, probando");
-        return new ResponseEntity<>("pojo_process", HttpStatus.BAD_REQUEST);
-    }
+	@RequestMapping(value = "/param_test")
+	public ResponseEntity<String> testParameters(@RequestParam(name = "name_x") String nameString,
+			@RequestParam(name = "name_int") int nameInteger, @RequestParam(name = "name_array") String[] theValues) {
+		// throw new RuntimeException("Probando, probando");
+		return new ResponseEntity<>("pojo_process", HttpStatus.BAD_REQUEST);
+	}
+
+	@RequestMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+	public App testJSON() {
+		return generateAppObject();
+	}
+
+	@RequestMapping(value = "/jolt")
+	public ModelAndView testJOLT() {
+		log.debug("Testing");
+		return new JoltModelAndView("jolt-test", generateAppObject(), HttpStatus.OK);
+	}
+	
+	private App generateAppObject() {
+		App app = new App(Arrays.asList(new MyPojo("anId", "aName"), new MyPojo("anotherId", "anotherName")));
+		return app;
+	}
 }
