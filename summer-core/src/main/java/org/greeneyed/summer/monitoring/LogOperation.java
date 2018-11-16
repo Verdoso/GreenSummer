b6faadaa9a3a5ce48a985684ba9d978b41b3e23a
verdoso <verdoso@gmail.com>
2018-11-16 10:52:52 +0100

    Created new annotation to log specific operations

---
+++ summer-core/src/main/java/org/greeneyed/summer/monitoring/LogOperation.java
@@ -0,0 +1,38 @@
+package org.greeneyed.summer.monitoring;
+
+/*
+ * #%L
+ * Summer
+ * %%
+ * Copyright (C) 2018 GreenEyed (Daniel Lopez)
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A marker annotation to specify that you DO want this method to be logged by LogOperationAspect, even if it is not a controller method.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogOperation {
+    String value();
+}
