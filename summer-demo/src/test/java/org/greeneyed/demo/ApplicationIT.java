6a62d0ff7784af10f17c1aca096970b0febf442e
verdoso <verdoso@gmail.com>
2019-09-09 11:50:26 +0200

    Added some code to the default test method

--- summer-demo/src/test/java/org/greeneyed/demo/ApplicationIT.java
+++ summer-demo/src/test/java/org/greeneyed/demo/ApplicationIT.java
@@ -1,5 +1,7 @@
 package org.greeneyed.demo;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class ApplicationIT {
 
 	@Test
 	public void contextLoads() {
+		assertTrue("Context is loaded", true);
 	}
 
 }
